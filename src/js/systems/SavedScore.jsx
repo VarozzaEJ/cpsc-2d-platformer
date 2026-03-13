ce23361892a09371708f741e2af94045b1f0a1b0
Evan Varozza <VarozzaEJ@gmail.com>
2026-03-13 15:14:25 -0600

    Add jsx files and add section to index.html for where score section will go

---
+++ src/js/systems/SavedScore.jsx
@@ -0,0 +1,10 @@
+export function SavedScore() {
+  return <></>;
+}
+
+(function start() {
+  const root = ReactDOM.createRoot(
+    document.getElementById("saved-score-inject"),
+  );
+  root.render(<SavedScore />);
+})();
